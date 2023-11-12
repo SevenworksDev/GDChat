127dda2d477c1aab1215970b6502f7d2e6599ade
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-11-12 15:44:54 -0800

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+$admins = ['7WORKS'];
+$hCaptchaSiteKey = '';
+$hCaptchaSecretKey = '';
+$regexfilter = '/^[a-zA-Z0-9!@#$%^&*()_\-+=|{}[\]:;\/?,. :]*$/';
+$banner = 'We now have Animated Emojis, Yippee! (Chat has been reset)';
+?>
